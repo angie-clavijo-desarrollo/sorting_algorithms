28f98e1c7435369f075e7189469cfe6c8e470bf9
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-06-14 14:35:12 -0500

    betty style file and comment function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,20 @@
 #include "sort.h"
-
-
-
+/**
+* bubble_sort - Function that sorted an array in ascending order
+* @array: is a parameter that will be sorted in order of less to greater
+* @size: Parameter of size of array
+* Return: Nothing
+**/
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for(j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			if(array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
