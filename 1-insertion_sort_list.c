a2f9d22780c841f04f93ab5e0c47e9ced37448cf
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-06-14 23:01:01 -0500

    task test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,15 +3,34 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *tmp = NULL, *aux = NULL, *node_next = NULL;
-    if (aux->n > aux->next->n)
+    listint_t *tmp = NULL, *aux = NULL;
 
-        tmp = (aux->next->n);
-        node_next->n = aux->n;
-        aux->n = tmp;
-        aux = aux->next;
-    else;
-        aux = aux->next
+    if (list == NULL || (*list)->next == NULL)
+    {
+        return;
+    }
+    aux = *list;
+    tmp = malloc(sizeof(listint_t));
+    if (tmp == NULL)
+    {
+        return;
+    }
+    while (aux->next != NULL)
+    {
+        if (aux->n > aux->next->n)
+        {
+            tmp->next = aux->next;
+            tmp->prev = aux->prev;
+            aux->next = aux->next->next;
+            aux->prev = tmp->next;
+            tmp->next->next = aux;
+            tmp->next->prev = tmp->prev;
+            *list = tmp->next;
+            print_list(*list);
 
+        }
+    aux = tmp->next->next;
+    }
 
+   printf("this is first node %d", (aux)->n);
 }
