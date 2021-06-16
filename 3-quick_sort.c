5a167e04d63ba13a34e9dc6d3863e17bd3881d9a
stevengm45 <stevengm45@gmail.com>
2021-06-16 19:01:40 +0000

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+int partition(int *array, int start, int end, size_t size)
+{
+	int i = 0, j, pivote = 0, tmp = 0;
+
+	i = start - 1;
+	pivote = array[end];
+	for (j = start; j <= end - 1; j++)
+	{
+		if (array[j] < pivote)
+		{
+			i++;
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			if (i != j)
+			{
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] != array[end])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[end];
+		array[end] = tmp;
+		if (i + 1 != j)
+		{
+			print_array(array, size);
+		}
+	}
+	return (i + 1);
+}
+
+
+
+void execute_quick_sort(int *array, int start, int end, size_t size)
+{
+	int pivot = 0;
+
+	if (start < end)
+	{
+		pivot = partition(array, start, end, size);
+		execute_quick_sort(array, start, pivot - 1, size);
+		execute_quick_sort(array, pivot + 1, end, size);
+	}
+}
+
+
+void quick_sort(int *array, size_t size)
+{
+	int start = 0, end = 0;
+
+	if (array == NULL)
+	{
+		return;
+	}
+	if (size < 2)
+	{
+		return;
+	}
+	end = size - 1;
+	execute_quick_sort(array, start, end, size);
+}
