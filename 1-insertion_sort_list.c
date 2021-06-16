90485f9e46e53ea709c6151ea2586463a964349b
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-06-15 23:55:37 -0500

    the insertion algorithms for doubly list linked

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,44 @@
 #include "sort.h"
-
+/**
+ * insertion_sort_list - Function that ordered doubly linked list,
+ * that oredered acsending
+ * @list: Paremeter doubly linked lict
+ * Return - None
+ */
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *forward = NULL, *backward = NULL, *a = NULL, *b = NULL, *c = NULL, *d = NULL;
+	listint_t *forward = NULL, *backward = NULL,
+				*a = NULL, *b = NULL, *c = NULL, *d = NULL;
 
-    if (list == NULL || (*list) == NULL || (*list)->next == NULL)
-    {
-        return;
-    }
-    forward = *list;
-    while (forward != NULL)
-    {
-        backward = forward;
-        while (backward != NULL && backward->prev != NULL && backward->prev->n > backward->n)
-        {
-            a = backward->prev->prev;
-            b= backward->prev;
-            c = backward;
-            d= backward->next;
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+		return;
+	forward = *list;
+	while (forward != NULL)
+	{
+		backward = forward;
+		while (backward != NULL
+				&& backward->prev != NULL && backward->prev->n > backward->n)
+		{
+			a = backward->prev->prev;
+			b = backward->prev;
+			c = backward;
+			d = backward->next;
 
-            if (a)
-            {
-                a->next = c;
-            }
-            c->next = b;
-            b->next = d;
+			if (a)
+				a->next = c;
+			c->next = b;
+			b->next = d;
 
-            d->prev = b;
-            if (d)
-            {
-                d->prev = b;
-            }
-            b->prev = c;
-            c->prev = a;
-            if (*list == b)
-            {
-                *list = c;
-            }
-            print_list(*list);
-            (void)10;
-
-           /* backward = backward->prev;*/
-        }
-        forward = forward->next;
-    }
+			if (d)
+				d->prev = b;
+			b->prev = c;
+			c->prev = a;
+			if (*list == b)
+				*list = c;
+			print_list(*list);
+			(void)10;
+		}
+		forward = forward->next;
+	}
 }
